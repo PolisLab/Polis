719d2bade2cb9b479339c599bc112dcb35fec6de
timjj92 <timjj92@gmail.com>
2019-11-09 10:43:10 -0800

    stockBoxes with function to display based on user's search

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,19 @@
 import React, {Component} from 'react'
-
+import StockList from '../container/StockList.jsx'
 
 
 class App extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-
-        }
+  constructor(props){
+    super(props);
+    this.state={
     }
-    render(){
-        return
-    }
+  }
+  render(){
+    return (
+      <div>
+        <StockList name={"tesl"}/>
+      </div>
+    )
+  }
 }
 export default App;
---
+++ client/container/StockList.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import StockBox from '../components/StockBox.jsx'
+
+const StockList = (props) => {
+  const [state, setState] = useState({counter:0});
+  let snp500 = ['apple', 'microsoft', 'tesla', 'test'];
+
+  const arrToShow = [];
+  for(let i = 0; i < snp500.length ; i++){
+    if(snp500[i].indexOf(props.name) !== -1)
+      arrToShow.push(<StockBox stockName={snp500[i]}/>);
+  }
+
+  return (
+    <div>
+      {arrToShow}
+    </div>
+  );
+}
+
+export default StockList;
