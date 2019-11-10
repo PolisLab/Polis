5e57d3ecf931bfa4264d6af08d991099cc467e43
unknown <sungdo1004@gmail.com>
2019-11-09 18:33:37 -0800

    some minor changes

--- server/models/polisModels.js
+++ server/models/polisModels.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://sungdo1004:entjddms0770@cluster0-2sjwa.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://sungdo1004:codesmith1004@cluster0-2sjwa.mongodb.net/test?retryWrites=true&w=majority';
+
+
 
 mongoose.connect(MONGO_URI, {
   // options for the connect method to parse the URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // sets the name of the DB that our collections are part of
-  dbName: 'starwars'
+  dbName: 'stock'
 })
 .then(()=>console.log(`Connected to Sung's Mongo DB`))
 .catch(err=>console.log(err));
@@ -32,6 +34,8 @@ const favStockSchema = new Schema({
     },
     favStockId : String
 })
+
+
 
 const Fav = mongoose.model('favs', favStockSchema);
 
