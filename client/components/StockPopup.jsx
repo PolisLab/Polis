5f87966c5052059ff43f67deb6e5741b10ac8d2f
timjj92 <timjj92@gmail.com>
2019-11-10 20:53:10 -0800

    added react loading tool, added stock info

--- client/components/StockPopup.jsx
+++ client/components/StockPopup.jsx
@@ -1,27 +1,49 @@
 import React, {useState, useEffect} from 'react';  
 import StockInfoDisplay from './StockInfoDisplay.jsx'
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const StockPopup = (props) =>{
   let price =0;
   let stockData= {};
+  const [isLoading, updateLoad] = useState(true);
 
   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${props.symbol}&interval=10min&outputsize=full&apikey=VRFP7Q7L5C1DU3EH`)
-  .then(body => body.json())
-  .then(body => {
-    stockData = body["Time Series (10min)"];
-    console.log(stockData);
-  })
-  .catch(err => console.log(err));
+ .then(body => body.json())
+ .then(body => {
+   updateLoad(false);
+   stockData = body["Time Series (10min)"];
+   console.log(body);
+ })
+ .catch(err => console.log(err));
+
+  const override = `
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  `;
 
   const handleSave = () =>{
     props.closePopup();
   };
 
   return (  
-    <div className='popup' onClick={handleSave}>  
+    <div className='popup' >  
       <div className='popup_inner'>  
-        <StockInfoDisplay data={stockData}/>
-        <h1>{price}!</h1>
+        {isLoading ? 
+        <div className='sweet-loading'>
+          <ClipLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={'#123abc'}
+            loading={isLoading}
+          />
+        </div> : 
+        <div>
+          <p>{props.companyName},{props.symbol} Today's Price{price}!</p><button>Favorite</button>
+          <StockInfoDisplay data={stockData} stockName={props.companyName} stockSymbol={props.symbol}/>
+          <span className= "closeButton" onClick={handleSave}>X</span>
+        </div>}
       </div>  
     </div>  
   );
