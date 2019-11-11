427db3f3676125254114d870ee5339d973318d98
evanking11 <evanking11@gmail.com>
2019-11-10 21:04:08 -0800

    stocks router

---
+++ server/routes/stocksRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const stocksController = require('../controllers/stocksController');
+
+const router = express.Router();
+
+router.get(
+  '/stocks/:id',
+  stocksController.getBuys,
+  stocksController.getFavs,
+  (req, res) => {
+    res.status(200).json({ stocks: res.locals.buys, favs: res.locals.favs });
+  }
+);
+
+router.post(
+  '/stocks/buys/:id',
+  stocksController.getBuys,
+  stocksController.addBuy,
+  (req, res) => {
+    res.status(200).json({ stocks: res.locals.buys });
+  }
+);
+
+router.delete(
+  '/stocks/buys/:id',
+  stocksController.getBuys,
+  stocksController.deleteBuy,
+  (req, res) => {
+    res.status(200).json({ stocks: res.locals.buys });
+  }
+);
+
+router.post(
+  '/stocks/favs/:id',
+  stocksController.getFavs,
+  stocksController.addFav,
+  (req, res) => {
+    res.status(200).json({ stocks: res.locals.favs });
+  }
+);
+
+router.delete(
+  '/stocks/favs/:id',
+  stocksController.getFavs,
+  stocksController.deleteFav,
+  (req, res) => {
+    res.status(200).json({ stocks: res.locals.favs });
+  }
+);
--- server/server.js
+++ server/server.js
@@ -4,14 +4,18 @@ const app = express();
 const PORT = 3000;
 const cookieParser = require('cookie-parser');
 
-app.use(express.static(path.resolve(__dirname, '../static/images')));
+app.use('/build', express.static(path.join(__dirname, '../build')));
+
 app.use(cookieParser());
 app.use(express.json());
-app.use('/', (req,res) => {
-    res.status(200).sendFile(path.resolve(__dirname,'../index.html'));
-  })
+app.use('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+app.use('/stocks');
+
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
-  });
-  
-  module.exports = app;
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
