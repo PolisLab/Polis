90e13ecefc72a8e73e9e4517398d025256457fb1
evanking11 <evanking11@gmail.com>
2019-11-11 16:03:17 -0800

    retrieve stock date and price

--- server/controllers/stocksController.js
+++ server/controllers/stocksController.js
@@ -1,10 +1,11 @@
-const db = require('../models/polisModel');
+const models = require('../models/polisModels');
+const fetch = require('node-fetch');
 
 const stocksController = {};
 
 stocksController.getBuys = (req, res, next) => {
-  const id = req.params.id;
-  Buy.find({ userID: id }, (err, buys) => {
+  const { email_address } = req.body;
+  models.Buy.find({ email_address }, (err, buys) => {
     if (err)
       return next('Error in stocksController.getBuys: ' + JSON.stringify(err));
     res.locals.buys = buys;
@@ -12,68 +13,104 @@ stocksController.getBuys = (req, res, next) => {
   });
 };
 
-stocksController.getFavs = (req, res, next) => {
-  const id = req.params.id;
-  User.favorites.find({ userID: id }, (err, favs) => {
+// stocksController.getFavs = (req, res, next) => {
+//   const id = req.params.id;
+//   User.favorites.find({ userID: id }, (err, favs) => {
+//     if (err)
+//       return next('Error in stocksController.getFavs: ' + JSON.stringify(err));
+//     res.locals.favs = favs;
+//     return next();
+//   });
+// };
+
+stocksController.addBuy = (req, res, next) => {
+  const { email_address, boughtStockID } = req.body;
+  models.Buy.create({ email_address, boughtStockID }, (err, buys) => {
     if (err)
-      return next('Error in stocksController.getFavs: ' + JSON.stringify(err));
-    res.locals.favs = favs;
+      return next('Error in stocksController.addBuy: ' + JSON.stringify(err));
+    res.locals.buys = buys;
     return next();
   });
 };
 
-stocksController.addBuy = (req, res, next) => {
-  const id = req.params.id;
-  Buy.create(
-    { userID: id, boughtStockID: req.body.boughtStockID },
-    (err, buys) => {
-      if (err)
-        return next('Error in stocksController.addBuy: ' + JSON.stringify(err));
-      res.locals.buys = buys;
-      return next();
-    }
-  );
-};
-
 stocksController.deleteBuy = (req, res, next) => {
-  const id = req.params.id;
-  Buy.remove(
-    { userID: id, boughtStockID: req.body.boughtStockID },
-    (err, buys) => {
-      if (err)
-        return next(
-          'Error in stocksController.deleteBuy: ' + JSON.stringify(err)
-        );
-      res.locals.buys = buys;
-      return next();
-    }
-  );
+  const { email_address, boughtStockID } = req.body;
+  models.Buy.remove({ email_address, boughtStockID }, (err, buys) => {
+    if (err)
+      return next(
+        'Error in stocksController.deleteBuy: ' + JSON.stringify(err)
+      );
+    res.locals.buys = buys;
+    return next();
+  });
 };
 
-stocksController.addFav = (req, res, next) => {
-  const id = req.params.id;
-  User.favorites.create(
-    { userID: id, favorites: req.body.favorites },
-    (err, favs) => {
-      if (err)
-        return next('Error in stocksController.addFav: ' + JSON.stringify(err));
-      res.locals.favs = favs;
-      return next();
-    }
-  );
+stocksController.savePastStocks = (req, res, next) => {
+  // console.log('this shit works');
+  // console.log(req.body);
+  const { symbol } = req.body;
+  fetch(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=VRFP7Q7L5C1DU3EH`
+  )
+    .then(result => result.json())
+    .then(result => {
+      // console.log(result['Time Series (Daily)']);
+      const resultSymbol = result['Meta Data']['2. Symbol'];
+      const price = result['Time Series (Daily)']['4. close'];
+      const finalResult = [];
+      for (let key in result['Time Series (Daily)']) {
+        let innerObj = {};
+        innerObj[key] = result['Time Series (Daily)'][key]['4. close'];
+
+        finalResult.push(innerObj);
+      }
+      // console.log(finalResult);
+      // let bigObj = {
+      //   symbol: resultSymbol,
+      //   changes: finalResult
+      // };
+      // // console.log(bigObj);
+      // const { symbol, changes } = bigObj;
+      models.PastStock.create({
+        stockSymbol: symbol,
+        changes: finalResult
+      }).then(result => {
+        console.log('this should be whats saved to db', result);
+        res.locals.pastStock = result;
+        next();
+      });
+    })
+    .catch(err => console.log(err));
+  //build for in loop to pull out closing costs for each day
+  //check if stocks already exist in db
 };
 
-stocksController.deleteFav = (req, res, next) => {
-  const id = req.params.id;
-  Buy.remove(
-    { userID: id, boughtStockID: req.body.boughtStockID },
-    (err, favs) => {
-      if (err)
-        return next(
-          'Error in stocksController.deleteFav: ' + JSON.stringify(err)
-        );
-      res.locals.favs = favs;
-      return next();
-    }
-  );
-};
+// stocksController.addFav = (req, res, next) => {
+//   const id = req.params.id;
+//   User.favorites.create(
+//     { userID: id, favorites: req.body.favorites },
+//     (err, favs) => {
+//       if (err)
+//         return next('Error in stocksController.addFav: ' + JSON.stringify(err));
+//       res.locals.favs = favs;
+//       return next();
+//     }
+//   );
+// };
+
+// stocksController.deleteFav = (req, res, next) => {
+//   const id = req.params.id;
+//   Buy.remove(
+//     { userID: id, boughtStockID: req.body.boughtStockID },
+//     (err, favs) => {
+//       if (err)
+//         return next(
+//           'Error in stocksController.deleteFav: ' + JSON.stringify(err)
+//         );
+//       res.locals.favs = favs;
+//       return next();
+//     }
+//   );
+// };
+
+module.exports = stocksController;
--- server/models/polisModels.js
+++ server/models/polisModels.js
@@ -1,43 +1,53 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://sungdo1004:codesmith1004@cluster0-2sjwa.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URI =
+  'mongodb+srv://sungdo1004:codesmith1004@cluster0-2sjwa.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(MONGO_URI, {
-  // options for the connect method to parse the URI
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // sets the name of the DB that our collections are part of
-  dbName: 'stock'
-})
-.then(()=>console.log(`Connected to Sung's Mongo DB`))
-.catch(err=>console.log(err));
+mongoose
+  .connect(MONGO_URI, {
+    // options for the connect method to parse the URI
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // sets the name of the DB that our collections are part of
+    dbName: 'stock'
+  })
+  .then(() => console.log(`Connected to Sung's Mongo DB`))
+  .catch(err => console.log(err));
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email_address : {type: String, required: true},
-    password: {type: String, required: true},
-    first_name : {type:String, required:true},
-    last_name: String,
-    favorites: [String]
-})
+  email_address: { type: String, required: true },
+  password: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: String,
+  favorites: [String]
+});
 
 const User = mongoose.model('user', userSchema);
 
 const buySchema = new Schema({
-    userId : {
-        type : Schema.Types.ObjectId, 
-        ref: 'user'
-    },
-    boughtStockId : String,
-    date : Date,
-    purchasedPrice : Number,
-    prediction: Number
-})
+  email_address: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  boughtStockId: String,
+  date: Date,
+  purchasedPrice: Number,
+  prediction: Number
+});
 
-const Buy = mongoose.model('buy', buySchema)
+const Buy = mongoose.model('buy', buySchema);
+
+const pastStockSchema = new Schema({
+  stockSymbol: String,
+  changes: [Object]
+});
+
+const PastStock = mongoose.model('pastStocks', pastStockSchema);
 
 module.exports = {
-    User, 
-    Buy
-}
+  User,
+  Buy,
+  PastStock
+};
